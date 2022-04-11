bd8396e70585823c9b8ea9df2228668bcdcb4a96
qiaoshouzi <61568944+qiaoshouzi@users.noreply.github.com>
2022-04-11 15:40:04 +0800

    重构: http 更换为 express

--- index.js
+++ index.js
@@ -1,38 +1,47 @@
-const tf = require('@tensorflow/tfjs-node')
-const nsfw = require('nsfwjs')
+const tf = require('@tensorflow/tfjs-node');
+const nsfw = require('nsfwjs');
 
-const http = require("http");
-const app = http.createServer();
+const express = require('express');
+const app = express();
 // const fs = require("fs");
 
 const port = 3000;
 const nsfwLoad = nsfw.load();
 
-app.on("request", (req, res) => {
+function catchE(res, e) {
+    res.end(JSON.stringify({
+        'code': -1,
+        'msg': e.message,
+        'data': {}
+    }));
+};
+
+app.post('/nsfw', (req, res) => {
     try {
-        reqUrl = req.url.split("/")
-        if (reqUrl[1] == 'nsfw' && req.method == "POST") {
-            let data = [];
-            req.on("data", (chunk) => {
-                data.push(chunk)
-            })
-            req.on("end", () => {
+        let data = [];
+        req.on("data", (chunk) => {
+            try {
+                data.push(chunk);
+            } catch (e) {
+                catchE(res, e);
+            };
+        });
+        req.on("end", () => {
+            try {
                 let buffer = Buffer.concat(data);
                 // 保存图片
                 // fs.writeFile("./image.jpg", buffer, (err) => {
                 //     if (!err) {
-                //         res.end("ok")
-                //     }
-                // })
-
-                const image = tf.node.decodeImage(buffer)
+                //         res.end("ok");
+                //     };
+                // });
+                const image = tf.node.decodeImage(buffer);
                 nsfwLoad.then((model) => {
                     model.classify(image).then((predictions) => {
-                        let data = {}
+                        let data = {};
                         for (i of predictions) {
-                            data[i['className']] = Math.trunc(i['probability'] * 10000) / 100
-                                // console.log(i['className'], Math.trunc(i['probability'] * 10000) / 100)
-                        }
+                            data[i['className']] = Math.trunc(i['probability'] * 10000) / 100;
+                        };
                         res.end(JSON.stringify({
                             'code': 0,
                             'msg': '',
@@ -40,24 +49,15 @@ app.on("request", (req, res) => {
                         }));
                     });
                 });
-            })
-        } else {
-            res.end(JSON.stringify({
-                'code': -404,
-                'msg': 'Not Found',
-                'data': {}
-            }));
-        }
+            } catch (e) {
+                catchE(res, e);
+            };
+        });
     } catch (e) {
-        //未知错误
-        res.end(JSON.stringify({
-            'code': -1,
-            'msg': e.message,
-            'data': {}
-        }));
-    }
-})
+        catchE(res, e);
+    };
+});
 
 app.listen(port, () => {
     console.log("server run at ", port);
-})
+});
